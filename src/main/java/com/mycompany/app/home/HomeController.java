bd8c135da4b95547586c039f7dc29c8915dcd4dc
countercrow <hongoppa@naver.com>
2023-06-21 18:29:55 +0900

    스프링

---
+++ src/main/java/com/mycompany/app/home/HomeController.java
@@ -0,0 +1,14 @@
+package com.mycompany.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="/")
+	public String home() {
+		
+		return "home";
+	}
+	
+}
